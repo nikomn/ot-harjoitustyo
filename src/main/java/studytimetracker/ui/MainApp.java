cd5829110eb0ff34c75b6c8e2831392059385c58
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2019-12-15 20:02:09 +0200

    Laajennettu GUI käyttöliittymää

--- src/main/java/studytimetracker/ui/MainApp.java
+++ src/main/java/studytimetracker/ui/MainApp.java
@@ -22,7 +22,14 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.animation.AnimationTimer;
 import javafx.geometry.Orientation;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 import studytimetracker.dao.DBWriter;
 import studytimetracker.domain.Course;
 import studytimetracker.domain.User;
@@ -41,13 +48,9 @@ public class MainApp extends Application {
 
     // Gui Components
     private Scene courseListScene;
-    private Label userNameLabel, stopTimerLabel, loginErrorLabel
-            , newUserNameLabel, newUserErrorLabel;
+    private Label userNameLabel, stopTimerLabel, loginErrorLabel, newUserNameLabel, newUserErrorLabel;
     private TextField usernameField, newUserNameField;
-    private Button loginSceneButton, loginButton, startTimerButton
-            , stopTimerButton, pauseTimerButton, newUserSceneButton
-            , createUserButton, backToLoginButton, selectCourseButton;
-
+    private Button loginSceneButton, loginButton, startTimerButton, stopTimerButton, pauseTimerButton, newUserSceneButton, createUserButton, backToLoginButton, selectCourseButton, selectedCourseButton;
 
     @Override
     public void start(Stage window) throws Exception {
@@ -58,10 +61,11 @@ public class MainApp extends Application {
         Scene loginScene = buildLoginScene();
         Scene newUserScene = buildNewUserScene();
         Scene mainMenuScene = buildMainMenuScene();
+        Scene testScene = buildCourseMenuScene();
 
         window.setTitle("StudyTimeTracker!");
         window.setScene(loginScene);
-        //window.setScene(startScene);
+        //window.setScene(testScene);
         window.show();
 
         this.loginButton.setOnAction((event) -> {
@@ -76,11 +80,11 @@ public class MainApp extends Application {
         this.newUserSceneButton.setOnAction((event) -> {
             window.setScene(newUserScene);
         });
-        
+
         this.backToLoginButton.setOnAction((event) -> {
             window.setScene(loginScene);
         });
-        
+
         this.createUserButton.setOnAction((event) -> {
             String errorCheck = createNewUser(this.newUserNameField.getText());
             //System.out.println(errorCheck);
@@ -90,12 +94,11 @@ public class MainApp extends Application {
                 this.newUserErrorLabel.setText(errorCheck);
             }
         });
-        
+
         this.selectCourseButton.setOnAction((event) -> {
             Scene courseListScene = buildCourseListScene();
             window.setScene(courseListScene);
         });
-        
         
 
     }
@@ -123,7 +126,7 @@ public class MainApp extends Application {
         Scene loginScene = new Scene(loginPane);
         return loginScene;
     }
-    
+
     public Scene buildNewUserScene() {
         GridPane newUserPane = new GridPane();
         this.newUserNameLabel = new Label("Select a username");
@@ -131,7 +134,7 @@ public class MainApp extends Application {
         this.createUserButton = new Button("Create user");
         this.backToLoginButton = new Button("Back to login");
         this.newUserErrorLabel = new Label("");
-        
+
         newUserPane.add(this.newUserNameLabel, 0, 0, 2, 1);
         newUserPane.add(this.newUserNameField, 0, 1, 2, 1);
         newUserPane.add(this.createUserButton, 0, 2);
@@ -159,7 +162,7 @@ public class MainApp extends Application {
         Button logOutButton = new Button("Log out");
         logOutButton.setMaxWidth(140);
 
-        mainMenuPane.add(selectCourseButton, 0, 1);
+        mainMenuPane.add(this.selectCourseButton, 0, 1);
         mainMenuPane.add(addCourseButton, 0, 2);
         mainMenuPane.add(showOverviewButton, 0, 3);
         mainMenuPane.add(logOutButton, 0, 4);
@@ -170,23 +173,75 @@ public class MainApp extends Application {
         mainMenuPane.setHgap(10);
         mainMenuPane.setPadding(new Insets(20, 20, 20, 20));
         
-
+        
         Scene mainMenuScene = new Scene(mainMenuPane);
         return mainMenuScene;
     }
-    
+
     public Scene buildCourseListScene() {
-        FlowPane courseListPane = new FlowPane(Orientation.VERTICAL);
-        courseListPane.setColumnHalignment(HPos.LEFT);
-        Label courseLabel = new Label("COURSES");
-        courseListPane.getChildren().add(courseLabel);
+        ScrollPane courseListPaneScroll = new ScrollPane();
+        GridPane courseListPane = new GridPane();
+        Label courseLabel = new Label("COURSES:");
+        courseLabel.setFont(new Font("Arial", 30));
+        courseListPane.add(courseLabel, 0, 0);
+        int index = 1;
         for (Course c : getUsersCourses(this.loggedUser)) {
-            courseListPane.getChildren().add(new Button(c.getName()));
+            Button b = new Button(c.getName());
+            b.wrapTextProperty().setValue(true);
+            b.setMaxWidth(300);
+            b.setMinWidth(300);
+            b.setMinHeight(90);
+            b.setMaxHeight(90);
+            b.setOnAction((event) -> {
+                System.out.println("Nappia " + b.getText() + " painettu!"); 
+            });
+            courseListPane.add(b, 0, index);
+            index++;
         }
         
-        Scene courseListScene = new Scene(courseListPane);
+
+        courseListPaneScroll.setPrefSize(500, 320);
+        courseListPane.setPrefSize(450, 320);
+        courseListPane.setAlignment(Pos.CENTER);
+        courseListPane.setVgap(10);
+        courseListPane.setHgap(10);
+        courseListPane.setPadding(new Insets(20, 20, 20, 20));
+
+        courseListPaneScroll.setContent(courseListPane);
+
+        Scene courseListScene = new Scene(courseListPaneScroll);
         return courseListScene;
+
+    }
+
+    public Scene buildCourseMenuScene() {
+        NumberAxis xAx = new NumberAxis();
+        CategoryAxis yAx = new CategoryAxis();
+        BarChart<Number, String> courseBarChart = new BarChart<>(xAx, yAx);
+        courseBarChart.setTitle("Time tracked (hours)");
+        courseBarChart.setLegendVisible(false);
+        XYChart.Series trackedTime = new XYChart.Series();
+        trackedTime.getData().add(new XYChart.Data(120, "JYM"));
+        courseBarChart.getData().add(trackedTime);
         
+        courseBarChart.setMinSize(500, 120);
+        courseBarChart.setMaxSize(500, 120);
+        
+        GridPane courseMenuPane = new GridPane();
+        Button b1 = new Button("Test Button!");
+        courseMenuPane.add(b1, 0, 0);
+        courseMenuPane.add(courseBarChart, 0, 1);
+        
+        courseMenuPane.setPrefSize(500, 320);
+        courseMenuPane.setAlignment(Pos.CENTER);
+        courseMenuPane.setVgap(10);
+        courseMenuPane.setHgap(10);
+        courseMenuPane.setPadding(new Insets(20, 20, 20, 20));
+
+        //Scene courseScene = new Scene(courseBarChart, 500, 120);
+        Scene courseScene = new Scene(courseMenuPane);
+        return courseScene;
+
     }
 
     public boolean logUserIn(String userName) {
@@ -198,7 +253,7 @@ public class MainApp extends Application {
             return false;
         }
     }
-    
+
     public String createNewUser(String userName) {
         User user = new User(userName);
         if (this.users.contains(user)) {
@@ -211,10 +266,10 @@ public class MainApp extends Application {
             } catch (Exception e) {
                 return "Unexpected error while writing to database!";
             }
-            
+
         }
     }
-    
+
     public List<Course> getUsersCourses(User user) {
         List<Course> courseList = new ArrayList<>();
         int index = 0;
@@ -224,7 +279,7 @@ public class MainApp extends Application {
             }
             index++;
         }
-        
+
         return courseList;
     }
 
